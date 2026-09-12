e481054872111b1073e8b191ac9eb610ca5a0d75
Zerete <luissantacruzriveros@gmail.com>
2026-09-12 01:54:03 -0300

    limpiar pantalla principal y mover botones de sesion al navbar

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router'; 
+import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common'; 
+import { AuthService } from '../../services/auth.service'; 
 
 @Component({
-  imports: [RouterLink], 
+  imports: [RouterLink, CommonModule], 
   selector: 'app-navbar',
   styleUrl: './navbar.scss',
   templateUrl: './navbar.html',
 })
-export class Navbar {}
+export class Navbar {
+  authService = inject(AuthService); 
+
+  login() {
+    this.authService.login();
+  }
+
+  logout() {
+    this.authService.logout();
+  }
+}
